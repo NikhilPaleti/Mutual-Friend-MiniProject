081e607b8b6b32368e196abd9952539241490a02
par2909 <55894034+par2909@users.noreply.github.com>
2021-06-28 22:23:22 +0530

    Add files via upload

--- test1_userinput.cpp
+++ test1_userinput.cpp
@@ -47,6 +47,15 @@ void   level_friend(int relation[])
    return ;
 
 }
+void   level_one(int relation[])
+{          int s=this->user::uid;
+        for (auto u :connection[s])
+        {
+            relation[u]=relation[s]+1;
+            //cout<<relation[u]<<" ";
+        }
+
+   return ;}
 char *  retname()
 {
     return name ;
@@ -73,8 +82,28 @@ int retuid()
         cout << endl;
     }
 }
-void printmatrix(int mat[][m])
-{cout<<"\nABOVE RELATION WILL LOOK LIKE IN FORM OF MATRIX\n";
+
+void minimumrelation(int mat[][m])
+{   int rell[users][users];
+    for(int i=0;i<users;i++)
+    {
+        for(int j=0;j<users;j++)
+        {
+            if(i==j)
+            {rell[i][j]=0;}
+            else if(mat[i][j]==1)
+                {rell[i][j]=mat[i][j];}
+            else rell[i][j]=10000;
+        }
+
+    }
+    for(int k=0;k<users;k++){
+        for(int i=0;i<users;i++){
+        for(int j=0;j<users;j++){
+                rell[i][j]=min(rell[i][j],rell[i][k]+rell[k][j]);
+
+        }}}
+       cout<<"\nABOVE RELATION WILL LOOK LIKE IN FORM OF MATRIX\n";
 cout<<"   ";
 for(int i=0;i<users;i++)
     cout<<i+1<<" ";
@@ -83,9 +112,57 @@ for(int i=0;i<users;i++)
     cout<<"\n";
     cout<<i+1<<"  ";
     for(int j=0;j<users;j++)
-    cout<<mat[i][j]<<" ";;
+    cout<<rell[i][j]<<" ";;
 
 }
+
+
+
+}
+
+void printAllPathsUtil(int u, int d, bool visited[],
+                              int path[], int& path_index)
+{
+    visited[u] = true;
+    path[path_index] = u;
+    path_index++;
+
+    if (u == d) {
+        for (int i = 0; i < path_index; i++)
+            cout << path[i] << " ";
+        cout << endl;
+    }
+    else
+    {
+
+        //list<int>::iterator i;
+        for (auto i = connection[u].begin(); i != connection[u].end(); ++i)
+            if (!visited[*i])
+                printAllPathsUtil(*i, d, visited, path, path_index);
+    }
+    path_index--;
+    visited[u] = false;
+}
+void printAllPaths(int s, int d)
+{
+    bool* visited = new bool[users+1];
+    int* path = new int[users+1];
+    int path_index = 0;
+    for (int i = 0; i < users+1; i++)
+        visited[i] = false;
+printAllPathsUtil(s, d, visited, path, path_index);
+}
+void commonfriend(int s, int d , int relation[])
+{
+    for (auto it = connection[s].begin();it != connection[s].end(); it++){
+        for (auto ti = connection[d].begin();ti != connection[d].end(); ti++){
+                if(*it==*ti){
+                   // cout<<*ti;
+                relation[*it]=1;}
+
+        }
+    }
+
 }
 
  int main()
@@ -115,30 +192,81 @@ for(int i=0;i<users;i++)
     }
 
    printcnt();
-    printmatrix(connection2);
-
-
-
-
-
-
-ch='y';
-while(ch=='y')
+    cout<<"\nABOVE RELATION WILL LOOK LIKE IN FORM OF MATRIX\n";
+cout<<"   ";
+for(int i=0;i<users;i++)
+    cout<<i+1<<" ";
+for(int i=0;i<users;i++)
 {
-cout<<"\n Enter the user id to obtain all level for friend connection ";
+    cout<<"\n";
+    cout<<i+1<<"  ";
+    for(int j=0;j<users;j++)
+    cout<<connection2[i][j]<<" ";;
+
+}
+
+
+
+
+
+
+int c;
+while(1)
+{   cout<<"\nenter your choice\n";
+    cout<<"1.To obtain all level for friend connection\n";
+    cout<<"2.To Find all the mutual friends between two people in the network\n";
+    cout<<"3.To Find the connection links between two people in the network\n";
+    cout<<"4.To Find the friends of a person in the network\n";
+    cout<<"5.To Find minimum relation matrix\n";
+    cout<<"6.EXIT\n";
+cin>>c;
+
+    switch(c){
+
+case 1 :{cout<<"\n Enter the user id to obtain all level for friend connection ";
 cin>>a;
 int rel[users];
-//cout<<account[a-1]->retname()<<" "<<account[a-1]->retuid()<<"\n ";
 account[a-1]->level_friend(rel);
-
-
 for(int i=1; i<=users;i++)
     {
         cout<<i<<" "<<account[i-1]->retname()<<" --> "<<rel[i]<<"\n";
     }
-cout<<"try again y/n ";
-cin>>ch;
- }
+    break;}
+ case 2 : {cout<<"enter the uid of users \n";
+ cin>>a>>b;
+ int rel[users];
+     memset(rel, 0, sizeof(rel));
+ commonfriend(a,b,rel);
+ for(int i=1; i<=users-1;i++)
+    {if(rel[i])
+        cout<<i<<" "<<account[i-1]->retname()<< "\n";
+    }
+
+break;}
+case 3 :{
+cout<<"enter the uid of users \n";
+ cin>>a>>b;
+     printAllPaths(a,b);
+break;}
+case 4 :{ cout<<"\n Enter the user id ";
+cin>>a;
+int rel[users];
+memset(rel, 0, sizeof(rel));
+account[a-1]->level_one(rel);
+for(int i=1; i<=users-1;i++)
+    {if(rel[i])
+        cout<<i<<" "<<account[i-1]->retname()<< "\n";
+    }
+break;}
+case 5 : {minimumrelation(connection2);
+break;}
+case 6 :{exit(1);}
+         default:
+    cout<<"\nInvalid type! \n";
+
+    }
+
+}
 
 
 return 0;
