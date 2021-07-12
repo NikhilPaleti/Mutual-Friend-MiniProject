eee912d80a23fc3a6c9546471cc48b1affde8cfd
Nikhil Paleti <paletinikhil@gmail.com>
2021-07-12 22:59:12 +0530

    Prettyfied the code

--- working.cpp
+++ working.cpp
@@ -1,6 +1,6 @@
-#include<bits/stdc++.h>
-#include<windows.h>
-#include<conio.h>
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <conio.h>
 using namespace std;
 
 #define users 10
@@ -8,28 +8,28 @@ using namespace std;
 //Intoduction of all
 void intro()
 {
-    cout<<"Press enter when everyone is ready ";
-getch();
-system("cls");
+    cout << "Press enter when everyone is ready ";
+    getch();
+    system("cls");
 
-const char msgWelcome[] =
-             "\t              *******************************************************\n\r"
-             "\t              *                                                     *\n\r"
-             "\t              *        Welcome to Social Network Project            *\n\r"
-             "\t              *                                                     *\n\r"
-             "\t              *******************************************************\n\r"
-             "\t              *                                                     *\n\r"
-             "\t              *                                                     *\n\r"
-             "\t              *        Team Members : Aman Bhadouria                *\n\r"
-             "\t              *                       Parish Bindal                 *\n\r"
-             "\t              *                       Nikhil Paleti                 *\n\r"
-             "\t              *                       Naivedya khare                *\n\r"
-             "\t              *                                                     *\n\r"
-             "\t              ******************************************************";
-             cout<< msgWelcome;
-system("intro.vbs");
-             getch();
-             system("cls");
+    const char msgWelcome[] =
+        "\t              *******************************************************\n\r"
+        "\t              *                                                     *\n\r"
+        "\t              *        Welcome to Social Network Project            *\n\r"
+        "\t              *                                                     *\n\r"
+        "\t              *******************************************************\n\r"
+        "\t              *                                                     *\n\r"
+        "\t              *                                                     *\n\r"
+        "\t              *        Team Members : Aman Bhadouria                *\n\r"
+        "\t              *                       Parish Bindal                 *\n\r"
+        "\t              *                       Nikhil Paleti                 *\n\r"
+        "\t              *                       Naivedya khare                *\n\r"
+        "\t              *                                                     *\n\r"
+        "\t              ******************************************************";
+    cout << msgWelcome;
+    system("intro.vbs");
+    getch();
+    system("cls");
 }
 void image()
 {
@@ -38,16 +38,16 @@ void image()
 void red()
 {
     HANDLE hConsole;
-     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-     SetConsoleTextAttribute(hConsole, 4);
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, 4);
 }
 void white()
 {
     HANDLE hConsole;
-     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-     SetConsoleTextAttribute(hConsole, 7);
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, 7);
 }
-void t2s(char* x)
+void t2s(char *x)
 {
     ofstream batch;
     batch.open("working.vbs", ios::out);
@@ -59,81 +59,84 @@ void t2s(char* x)
 
     system("working.vbs");
 }
-vector<int>connection[20];
+vector<int> connection[20];
 
-int user_in=0;
+int user_in = 0;
 
-
- class user
- {
-     int uid;
-    char name[20] ;
-    int Nfriends;
-public :
-    user( char *names)
-    {user_in++;
-
-        strcpy(name,names);
-        uid =user_in;
-    }
-void   level_friend(int relation[])
+class user
 {
-    queue<int> q;
-    bool visted[users+1];
-    for(int i = 0; i < users+1; i++)
-        visted[i] = false;
-       visted[this->user::uid]=true;
-    relation[this->user::uid]=0;
-    q.push(this->user::uid);
+    int uid;
+    char name[20];
+    int Nfriends;
 
-    while(!q.empty())
+public:
+    user(char *names)
     {
-        int s=q.front();
-        //cout<<s<<"==";
-        q.pop();
-        for (auto u :connection[s])
-        {
-          if (visted[u])continue;
-            visted[u]=true;
-            relation[u]=relation[s]+1;
-            //cout<<u<<" ";
-            q.push(u);
-        }
-    }
-   return ;
+        user_in++;
 
-}
-void   level_one(int relation[])
-{          int s=this->user::uid;
-        for (auto u :connection[s])
+        strcpy(name, names);
+        uid = user_in;
+    }
+    void level_friend(int relation[])
+    {
+        queue<int> q;
+        bool visted[users + 1];
+        for (int i = 0; i < users + 1; i++)
+            visted[i] = false;
+        visted[this->user::uid] = true;
+        relation[this->user::uid] = 0;
+        q.push(this->user::uid);
+
+        while (!q.empty())
         {
-            relation[u]=relation[s]+1;
+            int s = q.front();
+            //cout<<s<<"==";
+            q.pop();
+            for (auto u : connection[s])
+            {
+                if (visted[u])
+                    continue;
+                visted[u] = true;
+                relation[u] = relation[s] + 1;
+                //cout<<u<<" ";
+                q.push(u);
+            }
+        }
+        return;
+    }
+    void level_one(int relation[])
+    {
+        int s = this->user::uid;
+        for (auto u : connection[s])
+        {
+            relation[u] = relation[s] + 1;
             //cout<<relation[u]<<" ";
         }
 
-   return ;}
+        return;
+    }
 
+    char *retname()
+    {
+        return name;
+    }
+    int retuid()
+    {
+        return uid;
+    }
+};
+void printcnt()
+{
 
-char *  retname()
-{
-    return name ;
-}
-int retuid()
-{
-    return uid;
-}
- };
- void printcnt()
-{
-
-    for (int i = 1; i < users+1; i++) {
+    for (int i = 1; i < users + 1; i++)
+    {
 
         cout << "connection of uid "
              << i << ": ";
 
-
         for (auto it = connection[i].begin();
-             it != connection[i].end(); it++) {
+             it != connection[i].end(); it++)
+        {
 
             cout << *it << ' ';
         }
@@ -141,53 +144,61 @@ int retuid()
     }
 }
 void printmatrix(int mat[][users])
-{cout<<"\nABOVE RELATION WILL LOOK LIKE IN FORM OF MATRIX\n";
-cout<<"   ";
-for(int i=0;i<users;i++)
-    cout<<i+1<<" ";
-for(int i=0;i<users;i++)
 {
-    cout<<"\n";
-    cout<<i+1<<"  ";
-    for(int j=0;j<users;j++)
-    cout<<mat[i][j]<<" ";;
-
-}
+    cout << "\nABOVE RELATION WILL LOOK LIKE IN FORM OF MATRIX\n";
+    cout << "   ";
+    for (int i = 0; i < users; i++)
+        cout << i + 1 << " ";
+    for (int i = 0; i < users; i++)
+    {
+        cout << "\n";
+        cout << i + 1 << "  ";
+        for (int j = 0; j < users; j++)
+            cout << mat[i][j] << " ";
+        ;
+    }
 }
 void minimumrelation(int mat[][users])
-{   int rell[users][users];
-    for(int i=0;i<users;i++)
+{
+    int rell[users][users];
+    for (int i = 0; i < users; i++)
     {
-        for(int j=0;j<users;j++)
+        for (int j = 0; j < users; j++)
         {
-            if(i==j)
-            {rell[i][j]=0;}
-            else if(mat[i][j]==1)
-                {rell[i][j]=mat[i][j];}
-            else rell[i][j]=10000;
+            if (i == j)
+            {
+                rell[i][j] = 0;
+            }
+            else if (mat[i][j] == 1)
+            {
+                rell[i][j] = mat[i][j];
+            }
+            else
+                rell[i][j] = 10000;
         }
-
     }
-    for(int k=0;k<users;k++){
-        for(int i=0;i<users;i++){
-        for(int j=0;j<users;j++){
-                rell[i][j]=min(rell[i][j],rell[i][k]+rell[k][j]);
-
-        }}}
-        printmatrix(rell);
-
-
-
+    for (int k = 0; k < users; k++)
+    {
+        for (int i = 0; i < users; i++)
+        {
+            for (int j = 0; j < users; j++)
+            {
+                rell[i][j] = min(rell[i][j], rell[i][k] + rell[k][j]);
+            }
+        }
+    }
+    printmatrix(rell);
 }
 
 void printAllPathsUtil(int u, int d, bool visited[],
-                              int path[], int& path_index)
+                       int path[], int &path_index)
 {
     visited[u] = true;
     path[path_index] = u;
     path_index++;
 
-    if (u == d) {
+    if (u == d)
+    {
         for (int i = 0; i < path_index; i++)
             cout << path[i] << " ";
         cout << endl;
@@ -205,137 +216,146 @@ void printAllPathsUtil(int u, int d, bool visited[],
 }
 void printAllPaths(int s, int d)
 {
-    bool* visited = new bool[users+1];
-    int* path = new int[users+1];
+    bool *visited = new bool[users + 1];
+    int *path = new int[users + 1];
     int path_index = 0;
-    for (int i = 0; i < users+1; i++)
+    for (int i = 0; i < users + 1; i++)
         visited[i] = false;
-printAllPathsUtil(s, d, visited, path, path_index);
+    printAllPathsUtil(s, d, visited, path, path_index);
 }
-void commonfriend(int s, int d , int relation[])
+void commonfriend(int s, int d, int relation[])
 {
-    for (auto it = connection[s].begin();it != connection[s].end(); it++){
-        for (auto ti = connection[d].begin();ti != connection[d].end(); ti++){
-                if(*it==*ti){
-                   // cout<<*ti;
-                relation[*it]=1;}
-
+    for (auto it = connection[s].begin(); it != connection[s].end(); it++)
+    {
+        for (auto ti = connection[d].begin(); ti != connection[d].end(); ti++)
+        {
+            if (*it == *ti)
+            {
+                // cout<<*ti;
+                relation[*it] = 1;
+            }
         }
     }
-
 }
 
-
- int main()
- {
-     int z;
-     srand(time(0));
-     z= rand()%7 + 1;
+int main()
+{
+    int z;
+    srand(time(0));
+    z = rand() % 7 + 1;
 
     intro();
 
+    user *account[users];
+    cout << "the no of user in network =" << users << "\n";
+    int connection2[users][users];
+    memset(connection2, 0, sizeof(connection2));
 
+    char names[15][10] = {"pairsh", "rohit", "aman", "nikhil", "divyansh", "navi", "aniket", "shagun", "sanskar", "Apeeksha"};
+    vector<pair<int, int>> net = {{1, 2}, {1, 3}, {1, 6}, {2, 4}, {2, 6}, {3, 5}, {4, 8}, {5, 8}, {6, 10}, {7, 10}, {8, 9}, {10, 9}};
+    for (int i = 0; i < users; i++)
+    {
+        account[i] = new user(names[i]);
+    }
+    int a, b;
+    char ch = 'y';
 
-     user *account[users];
-     cout<<"the no of user in network ="<<users<<"\n";
-     int connection2[users][users];
-     memset(connection2, 0, sizeof(connection2));
-
-     char names[15][10]={"pairsh","rohit","aman","nikhil","divyansh","navi","aniket","shagun","sanskar","Apeeksha"};
-     vector<pair<int,int>> net={{1,2},{1,3},{1,6},{2,4},{2,6},{3,5},{4,8},{5,8},{6,10},{7,10},{8,9},{10,9}};
-     for(int i=0;i<users;i++)
-     {
-        account[i]=new user(names[i]);
-     }
-     int a ,b ;
-     char ch='y';
-
-        for(int i=0;i<12;i++){
-                a=net[i].first; b=net[i].second;
-        connection2[a-1][b-1]=connection2[b-1][a-1]=1;
+    for (int i = 0; i < 12; i++)
+    {
+        a = net[i].first;
+        b = net[i].second;
+        connection2[a - 1][b - 1] = connection2[b - 1][a - 1] = 1;
         connection[a].push_back(b);
         connection[b].push_back(a);
-   }
-   printcnt();
-   printmatrix(connection2);
+    }
+    printcnt();
+    printmatrix(connection2);
 
-//cout<<account[a-1]->retname()<<" "<<account[a-1]->retuid()<<"\n ";
+    //cout<<account[a-1]->retname()<<" "<<account[a-1]->retuid()<<"\n ";
 
-
-
-
-
-int c;
-while(1)
-{   cout<<"\nEnter your choice\n";
-    cout<<"1.To obtain all level for friend connection\n";
-    cout<<"2.To Find all the mutual friends between two people in the network";red();cout<<"(SAPI (text 2 speech) Enabled)\n";white();
-    cout<<"3.To Find the connection links between two people in the network\n";
-    cout<<"4.To Find the friends of a person in the network";red();cout<<"(SAPI (text 2 speech) Enabled)\n";white();
-    cout<<"5.To Find minimum relation matrix\n";
-     cout<<"6. Open the graph depicting the above relations\n";
-    cout<<"7.EXIT\n";
-cin>>c;
-char Q[20];
-    switch(c){
-
-case 1 :cout<<"\n Enter the user id to obtain all level for friend connection ";
-cin>>a;
-int rel[users];
-account[a-1]->level_friend(rel);
-for(int i=1; i<=users;i++)
+    int c;
+    while (1)
     {
-        cout<<i<<" "<<account[i-1]->retname()<<" --> "<<rel[i]<<"\n";
-    }
-    break;
- case 2: cout<<"enter the uid of users \n";
- cin>>a>>b;
-     memset(rel, 0, sizeof(rel));
- commonfriend(a,b,rel);
- for(int i=1; i<=users-1;i++)
-    {if(rel[i])
-{
-cout<<i<<" "<<account[i-1]->retname()<< "\n";
+        cout << "\nEnter your choice\n";
+        cout << "1.To obtain all level for friend connection\n";
+        cout << "2.To Find all the mutual friends between two people in the network";
+        red();
+        cout << "(SAPI (text 2 speech) Enabled)\n";
+        white();
+        cout << "3.To Find the connection links between two people in the network\n";
+        cout << "4.To Find the friends of a person in the network";
+        red();
+        cout << "(SAPI (text 2 speech) Enabled)\n";
+        white();
+        cout << "5.To Find minimum relation matrix\n";
+        cout << "6. Open the graph depicting the above relations\n";
+        cout << "7.EXIT\n";
+        cin >> c;
+        char Q[20];
+        switch (c)
+        {
 
-        t2s(account[i-1]->retname());}
-    }
+        case 1:
+            cout << "\n Enter the user id to obtain all level for friend connection ";
+            cin >> a;
+            int rel[users];
+            account[a - 1]->level_friend(rel);
+            for (int i = 1; i <= users; i++)
+            {
+                cout << i << " " << account[i - 1]->retname() << " --> " << rel[i] << "\n";
+            }
+            break;
+        case 2:
+            cout << "enter the uid of users \n";
+            cin >> a >> b;
+            memset(rel, 0, sizeof(rel));
+            commonfriend(a, b, rel);
+            for (int i = 1; i <= users - 1; i++)
+            {
+                if (rel[i])
+                {
+                    cout << i << " " << account[i - 1]->retname() << "\n";
 
-break;
-case 3:
-cout<<"enter the uid of users \n";
- cin>>a>>b;
-     printAllPaths(a,b);
-break;
-case 4: cout<<"\n Enter the user id ";
-cin>>a;
-memset(rel, 0, sizeof(rel));
-account[a-1]->level_one(rel);
-for(int i=1; i<=users-1;i++)
-    {if(rel[i]){
-        cout<<i<<" "<<account[i-1]->retname()<< "\n";
+                    t2s(account[i - 1]->retname());
+                }
+            }
 
-        t2s(account[i-1]->retname());
+            break;
+        case 3:
+            cout << "enter the uid of users \n";
+            cin >> a >> b;
+            printAllPaths(a, b);
+            break;
+        case 4:
+            cout << "\n Enter the user id ";
+            cin >> a;
+            memset(rel, 0, sizeof(rel));
+            account[a - 1]->level_one(rel);
+            for (int i = 1; i <= users - 1; i++)
+            {
+                if (rel[i])
+                {
+                    cout << i << " " << account[i - 1]->retname() << "\n";
 
+                    t2s(account[i - 1]->retname());
+                }
+            }
+            break;
+        case 5:
+            minimumrelation(connection2);
+            break;
+        case 6:
+            image();
+            break;
+        case 7:
+            exit(1);
+        default:
+        {
+            cout << "\nInvalid type! Did u mean option " << z << "?\n";
+            srand(time(0));
+            z = rand() % 7 + 1;
         }
-
+        }
     }
-break;
-case 5: minimumrelation(connection2);
-break;
-case 6:
-    image();
-    break;
-case 7 :exit(1);
-         default:
-    {cout<<"\nInvalid type! Did u mean option "<<z<<"?\n";
-srand(time(0));
-     z= rand()%7 + 1;
-    }
-
+    return 0;
 }
-
-}
-return 0;
- }
-
-
